cde23f09d4150fc85f7b8500d9f1474eac838d15
Kristy McLarnan <kristy2011@knights.ucf.edu>
2022-04-26 17:06:33 -0400

    Add files via upload

---
+++ MyRangeException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11C
+{
+    public class MyRangeException
+    {
+
+        // create method to check for loanAmount exception
+        public static void MyLoanAmount(double loan)
+        {
+            if (loan < 50000)
+            {
+                throw new ArgumentOutOfRangeException("The loan amount must be greater than $50,000.\n");
+            }
+
+        }
+
+        public static void MyLoanInterest(double interest)
+        {
+            if (interest <= 0)
+            {
+                throw new ArgumentOutOfRangeException("The interest rate must be at least 1%.\n");
+            }
+        }
+
+        public static void NumOfYears(double years)
+        {
+            if (years < 6)
+            {
+                throw new ArgumentOutOfRangeException("The loan must be at least 6 years.\n");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Assignment11C
+{
+    public class Program
+    {
+        static void Main(string[]args)
+        {
+
+            bool keepGoing = true;
+
+            MyRangeException re = new MyRangeException();
+
+            double loanInterest;
+            double years;
+            double loanAmount;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the loan amount: ");
+                    loanAmount = Convert.ToDouble(Console.ReadLine);
+                    MyRangeException.MyLoanAmount(loanAmount);
+                    keepGoing = false;
+
+                }
+                catch (FormatException formatException)
+                {
+                    Console.WriteLine("\n" + formatException.Message);
+                    Console.WriteLine("Double values only please.");
+
+                }
+                catch (ArgumentOutOfRangeException negativeNumberException)
+                {
+                    Console.Write("\n" + negativeNumberException.Message);
+
+                }
+
+
+            } while (keepGoing);
+
+            do
+            {
+                try
+                {
+                    Console.Write("Please enter the amount of years for the loan.");
+                    years = Convert.ToDouble(Console.ReadLine);
+                    MyRangeException.NumOfYears(years);
+                    keepGoing = false;
+                }
+                catch (FormatException formatException)
+                {
+                    Console.WriteLine(formatException.Message);
+                    Console.WriteLine("Please enter double numbers only.");
+
+                }
+                catch (ArgumentOutOfRangeException negativeNumbersException)
+                {
+                    Console.WriteLine(negativeNumbersException.Message);
+                    Console.WriteLine("Please enter positive numbers only.");
+                }
+            } while (keepGoing);
+
+            do
+            {
+                try
+                {
+                    Console.Write("Please enter the interest rate: ");
+                    loanInterest = Convert.ToDouble(Console.ReadLine);
+                    MyRangeException.MyLoanInterest(loanInterest);
+                    keepGoing = false;
+
+                }
+                catch (FormatException formatException)
+                {
+                    Console.WriteLine(formatException.Message);
+                    Console.WriteLine("Please enter only double numbers.");
+
+                }
+                catch (ArgumentOutOfRangeException negativeNumbersException)
+                {
+                    Console.WriteLine(negativeNumbersException.Message);
+                    Console.WriteLine("Positive numbers only.");
+                }
+
+
+
+
+            } while (keepGoing);
+    }
+    } }
+
+
+
+
+
+        
+    
